c4f94bf13a726ae5cc84dc7eb2cc2f07c8cd0d06
jobabyyy <joeannaavila123@gmail.com>
2022-12-15 16:31:43 -0800

    added header file

---
+++ binary_trees.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/** HERE WE CONATAIN OUR DATA STRUCTURES AND HEADER FILES **/
+
+/** DATA STRUCTURE FOR BINARY TREES **/
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
